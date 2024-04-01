d1e3adde3bb0da4e044d7df550dde0de20e758d0
unknown <athreyasudarshan@gmail.com>
2024-04-01 15:57:12 +0530

    merge conflict resolved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" /> */}
       </head>
       <body className={inter.className}>{children}
+        <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
       </body>
     </html>
   );
