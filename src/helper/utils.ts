58bba7ce944e25773ae8018b78996ef6282fa77c
unknown <athreyasudarshan@gmail.com>
2024-04-01 12:26:17 +0530

    first commit

---
+++ src/helper/utils.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const createNewTask = async (title: string, desc: string) => {
+    console.log("1111111111111")
+    try {
+      const resp = await axios.post('http://localhost:3001/api/todos', { title:title, description: desc, status: "todo" });
+      return resp;
+
+    } catch (error) {
+        console.error('Error creating task:', error);
+        return {error}
+    }
+};
---
+++ src/store/index.ts
@@ -0,0 +1,77 @@
+import create from 'zustand';
+import { persist,createJSONStorage } from 'zustand/middleware';
+
+type TaskList = {
+<<<<<<< HEAD
+  id:string;
+  title: string;
+  description: string;
+  showEdit: boolean;
+  status: string;
+=======
+    id:string;
+    title: string;
+    description: string;
+    showEdit: boolean;
+    status: string;
+>>>>>>> d3c462542e56ac9d297efd0be6af0d9d27531bc9
+};
+
+export const useTaskList = create(
+  persist(
+    (set) => ({
+      createdTasks: [],
+
+      allTask: (data: TaskList[]) => {
+        set((state: any) => ({
+            createdTasks: data
+        }))
+      },
+      updateTask: (id: string, title: string, description: string, showEdit: boolean, status: string) => {
+        set((state: any) => ({
+          createdTasks: [
+            ...state.createdTasks,
+            {
+              id: id,
+              title: title,
+              description: description,
+              showEdit: showEdit,
+              status:"todo"
+<<<<<<< HEAD
+            } as TaskList,   
+=======
+            } as TaskList,
+>>>>>>> d3c462542e56ac9d297efd0be6af0d9d27531bc9
+          ],
+        }));
+      },
+      editTodoTasks: (id: string, title: string, description: string, showEdit: boolean,status:string) => {
+        set((state: any) => ({
+          createdTasks:  state.createdTasks.map((task: any) =>
+          id === task.id
+            ? ({id: id,title: title, description: description, showEdit: showEdit, status:status} as TaskList)
+            : task
+        ),
+        }));
+      },
+      deleteTask: (id: string) => {
+        set((state: any) => ({
+          createdTasks: state.createdTasks.filter(
+            (data: any) => data.id !== id
+          ),
+        }));
+      },
+      deleteAlltasks:() =>{
+        set((state: any) => ({
+          createdTasks: state.createdTasks.filter(
+            (task: any) => task.status === "completed"
+          ),
+        }));
+      }
+    }),  
+    {
+      name: 'taskList', 
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
