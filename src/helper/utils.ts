9ffc159d6a923687257cf91b3cc66656b06f7c0f
unknown <athreyasudarshan@gmail.com>
2024-04-15 17:11:46 +0530

    New Commit

--- src/helper/utils.ts
+++ src/helper/utils.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
+import { socket } from "./socket";
 
-export const baseUrl = "https://final-api-todos.onrender.com"
+export const baseUrl = "http://localhost:3001"
+
 export const createNewTask = async (title: string, desc: string) => {
     try {
-      const resp = await axios.post(`${baseUrl}/api/todos`, { title:title, description: desc, status: "todo" });
-      return resp || {};
-
+      const resp = await axios.post(`${baseUrl}/api/todos/`, { title:title, description: desc, status: "todo" });
+        return resp || {};
+    
     } catch (error) {
         console.error('Error creating task:', error);
         return {error}
