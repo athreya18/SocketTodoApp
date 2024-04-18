d20caa005ec4802cfcc0bf73bf9f3a8139bbfe40
unknown <athreyasudarshan@gmail.com>
2024-04-18 11:13:48 +0530

    Last change

--- src/helper/utils.ts
+++ src/helper/utils.ts
@@ -7,10 +7,7 @@ export const baseUrl = "https://socketapi-2.onrender.com"
 export const createNewTask = async (title: string, desc: string) => {
     try {
       const resp = await axios.post(`${baseUrl}/api/todos/`, { title:title, description: desc, status: "todo" });
-      const socket=io();
-      socket.emit('newTask',resp.data);
-
-        return resp.data || {};
+        return resp|| {};
     } catch (error) {
         console.error('Error creating task:', error);
         return {error}
