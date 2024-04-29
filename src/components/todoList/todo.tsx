a240e0c99de857e19ee077f2cc9bbe94fd2067e6
unknown <athreyasudarshan@gmail.com>
2024-04-29 18:22:29 +0530

    Change

--- src/components/todoList/todo.tsx
+++ src/components/todoList/todo.tsx
@@ -180,7 +180,7 @@ const Todos = (props: any) => {
             const resp = await createNewTask(title, desc);
             const { data = {} }: any = resp
             console.log({data})
-            newTasks(data.id, data.title, data.description, false, data.status)
+            // newTasks(data.id, data.title, data.description, false, data.status)
             setTitle('');
             setDesc('');
             closeSheet();
